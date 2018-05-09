15a3a4c4144cea7d5b58ab50e156d854251e4f97
Wojtek <woltorb8@gmail.com>
2018-05-09 21:03:44 +0200

    100%

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function pasteBlock(name, foto, trackName) {
     var result = `
     <div class="block">
         <img src="${foto}" />
-        <div>
+        <div class="block-body">
         <p class="text1">${trackName}</p>
+        <div class="hr"></div>
         <p class="text2">${name}</p>
         </div>
-        
     </div>
     `
     return result;
@@ -14,44 +14,45 @@ function pasteBlock(name, foto, trackName) {
 
 function getAPI() {
 
-  
-    
-
     let value = $('#value1').val();
+    let list = $('#list');
 
     const API_URL = `https://itunes.apple.com/search?term=${value}`;
 
     $.ajax({
         url: API_URL,
         type: 'GET',
+        dataType: 'jsonp',
         crossDomain: true,
-        success: function (data) {
-            var params = data;
-            obj = JSON.parse(params);
-            console.log(obj);
-            for (var i = 0; i > obj.results.length; i++) {
-                console.log(obj.results[i])
+        success: data => {
+                console.log(data);
+            if(data.results.length === 0){
+                 $('#totalNumber').html('<center>Sorry, no matches found</center>');
+                 list.prev().html("");
+                 list.html("");
             }
-            var html = '';
-            $.each(obj.results, function (element, value) {
-                console.log(element);
-                html += pasteBlock(value.artistName, value.artworkUrl100, value.trackName);
+            else {
 
-            })
+           
 
-            $('#list').html('');
-            $('#list').innerHTML = "";
-            $('#list').append($.parseHTML(html))
+            list.pagination({
+                dataSource: data.results,
+                pageSize: 9,
+                showPageNumbers: false,
+                prevText:'<< Prev',
+                nextText:'Next >>',
 
+                callback: (response, pagination) => {
+                    console.log(response, pagination);
+                    $('#totalNumber').html(`<center>Found ${pagination.totalNumber} songs</center>`);
+                    let html = "";
+                    $.each(response, (index, item) => {
+                        html += pasteBlock(item.artistName, item.artworkUrl100, item.trackName);
+                    });
 
-
+                    list.prev().html(html);
+                }
+            }) }
         },
-       
-
     })
-
 }
-
-
-
-
